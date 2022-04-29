e12f2172166e16d98663574addbfc9fa54ec1aaf
Dante Benicio <dantecosta79@gmail.com>
2022-04-29 15:31:56 -0300

    orderned root comments and replies comments

--- src/utils/orderComments.ts
+++ src/utils/orderComments.ts
@@ -1,3 +1,12 @@
 export function orderComments(comments: any) {
-  return comments.sort((currentComment: any, nextComment: any) => currentComment.score - nextComment.score).reverse()
+  const rootCommentsOrderned = comments.sort((currentComment: any, nextComment: any) => currentComment.score - nextComment.score).reverse()
+  const allCommentsOrderned = rootCommentsOrderned.map((comment: any) => {
+    if (comment.replies.length) {
+      return { ...comment, replies: comment.replies.sort((a:any, b:any) => a.score - b.score).reverse() }
+    }
+
+    return {...comment}
+  })
+
+  return allCommentsOrderned
 }
