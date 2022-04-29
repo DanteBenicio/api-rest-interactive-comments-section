601adf2cc608a8e15432b62a9731c8fd6fe2578e
Dante Benicio <dantecosta79@gmail.com>
2022-04-29 15:36:21 -0300

    remove blank spaces

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,15 @@ import { connectMongodb } from './services/mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-// configurando para pegar as variaveis de ambiente
 dotenv.config()
 
-// inicializando o express
 const app = express();
 
-// conectar com o mongodb
 connectMongodb(process.env.MONGO_DB_URL!);
 
 // middlewares (funçoes que são executadas entre a requisição e resposta)
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
-
-app.use(cors())
-
 app.use('/', require('./routes'))
 
 app.listen(process.env.PORT || 3001, () => {
